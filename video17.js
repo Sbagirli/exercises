9f4c57017bd327ea1ddfdc610fc42a1bf7327fa9
Suleyman <hhhh@fsdfdsf.md>
2021-11-26 01:45:02 +0400

    19 video

---
+++ video17.js
@@ -0,0 +1,35 @@
+"use string";
+
+const str = "teSt" ;
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase()) ;
+console.log(str.toLowerCase()) ;
+
+
+console.log(str);
+
+
+const  fruit = 'Some fruit' ;
+
+console.log(fruit.indexOf("q")) ;
+
+
+const logger = "Hello world" ;
+
+// console.log(logger.slice(-5, -1)) ;
+
+
+// console.log(logger.substring(6, 11)) ;
+
+
+console.log(logger.substr(6, 5)) ;
+
+
+
+const num = 12.2 ;
+console.log(Math.round(num)) ;
+
+const test = "12.2px" ;
+console.log(parseFloat(test)) ;
