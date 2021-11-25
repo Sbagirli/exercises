fc916d3e8e428019aee1dbdc55270bc430663570
Suleyman <hhhh@fsdfdsf.md>
2021-11-26 00:49:42 +0400

    video 18

--- homework15/script.js
+++ homework15/script.js
@@ -1,6 +1,15 @@
 "use string"
 
-const numberOfFilms = prompt ('Skolko filmov vi uje posmotreli?' , '') ;
+let numberOfFilms = +prompt ('Skolko filmov vi uje posmotreli?' , '') ;
+
+function start() {
+    numberOfFilms = +prompt ('Skolko filmov vi uje posmotreli?' , '') ;  
+    
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt ('Skolko filmov vi uje posmotreli?' , '') ;  
+    }
+}
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -20,31 +29,64 @@ const personalMovieDB = {
 // personalMovieDB.movies[c] = d ;
 
 
-for (let i = 0; i < 2 ; i++) {
-    const a = prompt ('Odin iz poslednix prosmotrennix filmov?', '') ,
-          b = prompt ('Na skolko ocenite yeqo?' , '');
-
-    if(a != null && b != null && a!='' && b!= '' && a.length<50) {
-        personalMovieDB.movies[a] = b ;
-        console.log('done') ;
-    }  else {
-        console.log('error');
-        i--;
-
-    }
-   
+function rememberMyFilms() {
+    for (let i = 0; i < 2 ; i++) {
+        const a = prompt ('Odin iz poslednix prosmotrennix filmov?', '') ,
+              b = prompt ('Na skolko ocenite yeqo?' , '');
+    
+        if(a != null && b != null && a!='' && b!= '' && a.length<50) {
+            personalMovieDB.movies[a] = b ;
+            console.log('done') ;
+        }  else {
+            console.log('error');
+            i--;
+    
+        }  
+    }     
 }
 
+// remembetMyFilms() ;
 
 
-if(personalMovieDB.count< 10) {
-   console.log('Prosmotreno malo filmov') ;
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-   console.log('Vi klassicheskiy zritel');
-} else if (personalMovieDB.count >= 30) {
-   console.log('Vi kinoman')
-} else { 
-    console.log('Proizoshla oshibka')
+
+
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count< 10) {
+        console.log('Prosmotreno malo filmov') ;
+     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+        console.log('Vi klassicheskiy zritel');
+     } else if (personalMovieDB.count >= 30) {
+        console.log('Vi kinoman') ;
+     } else { 
+         console.log('Proizoshla oshibka') ;
+     }
+
+
+
 }
 
-console.log(personalMovieDB) ;
+// detectPersonalLevel() ;
+
+function showMyDB(hidden) {
+         if(!hidden) {
+            console.log(personalMovieDB) ;
+
+         }
+
+
+
+}
+
+showMyDB(personalMovieDB.privat) ;
+
+
+function writeYourGenres() {
+     for (let i = 1; i <= 3; i++) {
+                          personalMovieDB.genres[i - 1] = prompt (`Vash lubimiy janr po nomerom ${i}`) ;
+
+     }
+
+}
+
+writeYourGenres() ;
